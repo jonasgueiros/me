8527b274eabf23dfb184d31b82a61a541b4f1feb
Jonas <57268401+jonasgueiros@users.noreply.github.com>
2023-04-09 00:42:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.reveal1',{
+    rotate:{x: 100, y: 0, z: 0},
+    duration: 2000
+});
+sr.reveal('.card-container',{
+    rotate:{x: 100, y: 0, z: 0},
+    duration: 1000
+});
+sr.reveal('.text',{
+    rotate:{x: 100, y: 0, z: 0},
+    duration: 2000
+});
